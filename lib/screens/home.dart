a98df8108de0d2cd36a0e6a96eb5c88eb6367ee0
isthatkhensi <smkhensi@gmail.com>
2023-06-15 17:38:47 +0200

    Revert "Merge branch 'main' of https://github.com/YamkelaQhogwana/Scholarly"
    This reverts commit 126a85501a444959a3d33d21c05092eb0d0b8520, reversing
    changes made to 2db70bdabb794f0246e708e3d191493e6bfacf34.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,204 +1,95 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:scholarly/constants/colors.dart';
 import 'package:scholarly/screens/calendar.dart';
 import 'package:scholarly/screens/classes.dart';
 import 'package:scholarly/screens/info.dart';
-import 'package:scholarly/screens/feedback_form.dart';
-import 'package:scholarly/modules/events.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:scholarly/screens/habits_tab.dart';
-import 'package:scholarly/screens/tasks_tab.dart';
-import 'package:scholarly/screens/habits_tab.dart';
-import 'package:scholarly/screens/tasks_form.dart';
-import 'package:scholarly/screens/main_screen_menu.dart';
-import 'package:scholarly/constants/custom_appbar.dart';
 
-class Event {
-  final String title;
-
-  Event(this.title);
-}
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  CalendarFormat _calendarFormat = CalendarFormat.week;
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-  TabController? _tabController;
-  int _currentPageIndex = 0;
-
-  Map<DateTime, List<Event>> _events = {
-    DateTime(2023, 5, 30): [
-      Event('Event A'),
-      Event('Event B'),
-    ],
-    DateTime(2023, 5, 31): [
-      Event('Event C'),
-    ],
-    DateTime(2023, 6, 1): [
-      Event('Event D'),
-      Event('Event E'),
-    ],
-  };
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController?.dispose();
-    super.dispose();
-  }
-
-  void _changePage(int index) {
-    setState(() {
-      _currentPageIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
-      appBar: const CustomAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TableCalendar<Event>(
-            firstDay: DateTime.utc(2022, 1, 1),
-            lastDay: DateTime.utc(2024, 12, 31),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            },
-            eventLoader: (day) {
-              return _events[day] ?? [];
-            },
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-            headerStyle: const HeaderStyle(
-              formatButtonVisible: false, // Hide the format button
-              titleTextStyle: TextStyle(fontSize: 20),
-              titleCentered: true,
-              leftChevronIcon: Icon(Icons.chevron_left),
-              rightChevronIcon: Icon(Icons.chevron_right),
-            ),
-          ),
-          SizedBox(height: 16),
-          TabBar(
-            controller: _tabController,
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(text: 'Tasks'),
-              Tab(text: 'Habits'),
-            ],
-          ),
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: <Widget>[
-                TasksPage(),
-                Habits_Page(),
-              ],
-            ),
-          ),
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        child: Text("Main Screen/Home Page"),
       ),
       floatingActionButton: Stack(
-        children: [
-          Positioned(
-            bottom: kBottomNavigationBarHeight / 2,
-            left: (MediaQuery.of(context).size.width - 56) / 2,
-            child: Container(
-              width: 56,
-              height: 56,
-              decoration: BoxDecoration(
-                color: AppColors.kPrimary400,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  showTaskFormBottomSheet(context);
-                },
-                icon: Icon(Icons.add, color: Colors.white),
-              ),
+      children: [
+        Positioned(
+          bottom: kBottomNavigationBarHeight / 2,
+          left: (MediaQuery.of(context).size.width - 56) / 2, // Adjust the left position
+          child: Container(
+            width: 56,
+            height: 56,
+            decoration: BoxDecoration(
+              color: AppColors.kPrimary400, // Set your desired button color here
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: IconButton(
+              onPressed: () {
+                // Placeholder code for add button
+                print('Add button pressed');
+              },
+              icon: Icon(Icons.add, color: Colors.white), // Set the icon color
             ),
           ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              color: _currentPageIndex == 0
-                  ? AppColors.kPrimary400
-                  : AppColors.kMainText,
-              onPressed: () {
-                _changePage(0);
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.calendar_month),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CalendarPage()),
-                );
-              },
-            ),
-            SizedBox(width: 56), // Empty space for the float button
-            IconButton(
-              icon: Icon(Icons.school),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ClassesPage()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.newspaper),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InfoPage()),
-                );
-              },
-            ),
-          ],
         ),
+      ],
+    ),
+floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+bottomNavigationBar: BottomAppBar(
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      IconButton(
+        icon: Icon(Icons.home),
+        color: AppColors.kPrimary400,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
       ),
-    );
-  }
+      IconButton(
+        icon: Icon(Icons.calendar_month),
+        color: AppColors.kMainText,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CalendarPage()),
+          );
+        },
+      ),
+      SizedBox(width: 56), // Empty space for the float button
+      IconButton(
+        icon: Icon(Icons.school),
+        color: AppColors.kMainText,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ClassesPage()),
+          );
+        },
+      ),
+      IconButton(
+        icon: Icon(Icons.newspaper),
+        color: AppColors.kMainText,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => InfoPage()),
+          );
+        },
+      ),
+    ],
+  ),
+),
+);
+  
+}
 }
