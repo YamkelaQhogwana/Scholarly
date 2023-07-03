7ab6347aa05a3336939fc08d6df55d03ff2001b1
Khensi Malaeka <60666683+isthatkhensi@users.noreply.github.com>
2023-07-04 00:54:34 +0200

    updated stats page

--- lib/screens/menu/statistics.dart
+++ lib/screens/menu/statistics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:scholarly/constants/colors.dart';
 import 'package:scholarly/modules/stats.dart';
 import 'package:scholarly/screens/calendar.dart';
@@ -12,139 +13,76 @@ class MenuStatistics extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(color: AppColors.kMainText, size: 18),
-        title: const Text('Statistics',
-            style: TextStyle(color: AppColors.kMainText, fontSize: 16)),
-        backgroundColor: const Color.fromARGB(255, 248, 248, 248),
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Overview',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15)),
-            const SizedBox(height: 10),
-            Row(
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      future: FirebaseFirestore.instance.collection('statistics').doc('No2dz33RIoc47qFqOTWk').get(),
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Center(
+            child: Text('No data found.'),
+          );
+        }
+
+        final data = snapshot.data!.data()!;
+        final completedHabitsCount = data['completedHabitsCount'] ?? 0;
+        final completedTasksCount = data['completedTasksCount'] ?? 0;
+        final habitsThisWeek = data['habitsThisWeek'] ?? '';
+
+        return Scaffold(
+          appBar: AppBar(
+            iconTheme: const IconThemeData(color: AppColors.kMainText, size: 18),
+            title: const Text(
+              'Statistics',
+              style: TextStyle(color: AppColors.kMainText, fontSize: 16),
+            ),
+            backgroundColor: const Color.fromARGB(255, 248, 248, 248),
+            elevation: 0,
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                _buildStatBlock(
-                  'Completed Tasks',
-                  '${DummyStatsData.totalTasksCompleted}',
+                const Text(
+                  'Overview',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
                 ),
-                const SizedBox(height: 16.0),
-                _buildStatBlock(
-                  'Total Habits',
-                  '${DummyStatsData.currentHabitsTracked}',
-                ),
-                const SizedBox(height: 16.0),
-                _buildStatBlock(
-                  'Highest Habit Streak',
-                  '${DummyStatsData.highestHabitStreak}',
-                ),
-                const SizedBox(height: 24.0),
-              ],
-            ),
-            const SizedBox(height: 25.0),
-            BarGraphWidget(DummyStatsData.tasksCompletedWeek),
-            const SizedBox(height: 10.0),
-            BarGraphWidget(DummyStatsData.habitsCompletedWeek),
-            const SizedBox(height: 25.0),
-            const Text('Habit Streaks',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15)),
-            const SizedBox(height: 10),
-            Wrap(
-              spacing: 1,
-              children: [
-                for (HabitTracker habitTracker in DummyStatsData.habitTrackers)
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: _buildStatBlock(
-                      habitTracker.name,
-                      habitTracker.streak.toString(),
+                const SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildStatBlock(
+                      'Completed Tasks',
+                      completedTasksCount.toString(),
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
+                    _buildStatBlock(
+                      'Completed Habits',
+                      completedHabitsCount.toString(),
+                    ),
+                    const SizedBox(height: 24.0),
+                  ],
+                ),
+                const SizedBox(height: 25.0),
+                BarGraphWidget(habitsThisWeek),
               ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Stack(
-        children: [
-          Positioned(
-            bottom: kBottomNavigationBarHeight / 2,
-            left: (MediaQuery.of(context).size.width - 56) / 2,
-            child: Container(
-              width: 56,
-              height: 56,
-              decoration: BoxDecoration(
-                color: AppColors.kPrimary400,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  // Placeholder code for add button
-                  print('Add button pressed');
-                },
-                icon: const Icon(Icons.add, color: Colors.white),
-              ),
             ),
           ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.home),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.calendar_month),
-              color: AppColors.kPrimary400,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CalendarPage()),
-                );
-              },
-            ),
-            const SizedBox(width: 56),
-            IconButton(
-              icon: const Icon(Icons.school),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ClassesPage()),
-                );
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.newspaper),
-              color: AppColors.kMainText,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InformationCentre()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+          // Rest of the code...
+        );
+      },
     );
   }
 
@@ -162,17 +100,19 @@ class MenuStatistics extends StatelessWidget {
           Text(
             value,
             style: const TextStyle(
-                fontSize: 38.0,
-                fontWeight: FontWeight.bold,
-                color: AppColors.kPrimary400),
+              fontSize: 38.0,
+              fontWeight: FontWeight.bold,
+              color: AppColors.kPrimary400,
+            ),
           ),
           Text(
             label,
             textAlign: TextAlign.center,
             style: const TextStyle(
-                fontSize: 12.0,
-                color: AppColors.kMainText,
-                fontWeight: FontWeight.w500),
+              fontSize: 12.0,
+              color: AppColors.kMainText,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ],
       ),
@@ -181,14 +121,16 @@ class MenuStatistics extends StatelessWidget {
 }
 
 class BarGraphWidget extends StatelessWidget {
-  final Map<String, int> data;
+  final String habitsThisWeek;
 
-  BarGraphWidget(this.data);
+  const BarGraphWidget(this.habitsThisWeek);
 
   @override
   Widget build(BuildContext context) {
-    List<DataPoint> chartData =
-        data.entries.map((entry) => DataPoint(entry.value, entry.key)).toList();
+    final dataPoints = habitsThisWeek.split(',').map((value) => int.tryParse(value) ?? 0).toList();
+    final List<DataPoint> chartData = List.generate(dataPoints.length, (index) {
+      return DataPoint(index, dataPoints[index]);
+    });
 
     return Container(
       width: 350,
@@ -201,10 +143,10 @@ class BarGraphWidget extends StatelessWidget {
         primaryXAxis: CategoryAxis(),
         primaryYAxis: NumericAxis(),
         series: <ChartSeries>[
-          BarSeries<DataPoint, String>(
+          BarSeries<DataPoint, int>(
             dataSource: chartData,
-            xValueMapper: (DataPoint data, _) => data.y,
-            yValueMapper: (DataPoint data, _) => data.x,
+            xValueMapper: (DataPoint data, _) => data.x,
+            yValueMapper: (DataPoint data, _) => data.y,
           ),
         ],
       ),
@@ -213,8 +155,8 @@ class BarGraphWidget extends StatelessWidget {
 }
 
 class DataPoint {
-  final String y;
   final int x;
+  final int y;
 
   DataPoint(this.x, this.y);
 }
