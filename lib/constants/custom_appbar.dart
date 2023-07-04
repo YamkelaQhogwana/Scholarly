50c50227b31ad31287ffad4306f38599c40c9388
claudia <claudiabasson13@gmail.com>
2023-07-04 08:13:23 +0200

    profile icon

--- lib/constants/custom_appbar.dart
+++ lib/constants/custom_appbar.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:scholarly/constants/colors.dart';
 import 'package:intl/intl.dart';
@@ -6,8 +8,54 @@ import 'package:iconify_flutter/icons/ci.dart';
 import 'package:scholarly/screens/main_screen_menu.dart';
 import 'package:scholarly/screens/menu/statistics.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   const CustomAppBar({Key? key}) : super(key: key);
+
+
+  @override
+  _CustomAppBarState createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+  }
+
+  class _CustomAppBarState extends State<CustomAppBar> {
+  late String userName = '';
+  late List<Map<String, dynamic>> modules;
+  late String icon = '';
+
+  @override
+  void initState() {
+  super.initState();
+  fetchUserData();
+  }
+
+  void fetchUserData() async {
+  try {
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+  QuerySnapshot snapshot = await FirebaseFirestore.instance
+      .collection('users')
+      .where('email', isEqualTo: user.email)
+      .limit(1)
+      .get();
+  if (snapshot.docs.isNotEmpty) {
+  DocumentSnapshot userDoc = snapshot.docs.first;
+  Map<String, dynamic>? data =
+  userDoc.data() as Map<String, dynamic>?;
+  if (data != null) {
+  setState(() {
+  userName = data['fname'] ?? '';
+  icon = data['icon'] ?? '';
+  });
+  }
+  }
+  }
+  } catch (e) {
+  print('Error fetching user data: $e');
+  }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +72,10 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         height: 60,
         child: Row(
           children: [
-            const CircleAvatar(
+             CircleAvatar(
               radius: 30,
               backgroundImage:
-                  AssetImage('assets/images/avatars/black-wn-av.png'),
+                  AssetImage(icon),
               backgroundColor: Color.fromARGB(255, 255, 255, 255),
             ),
             const SizedBox(width: 10),
